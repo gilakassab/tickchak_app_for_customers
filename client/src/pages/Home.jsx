6d27a85920a7ff847f349b4320d0da7401c244c6
gilakassab <gilakassab@gmail.com>
2024-08-23 16:40:50 +0300

    Final stage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React ,{useState, useEffect}from 'react';
+import React from 'react';
 import MainHeader from '../components/MainHeader';
 import SecondHeader from '../components/SecondHeader';
 import Events from '../components/Events';
@@ -6,32 +6,11 @@ import AboutUs from '../components/AboutUs';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
-let alertShown = false;
+
 
 
 function Home() {
   
-
-  useEffect(() => {
-    // Check if the alert has already been shown
-    if (!alertShown) {
-      alert(`To use this website, you need to set the following variables in your .env file:
-
-        1. ACCESS_TOKEN_SECRET: A string for securing access tokens.
-        2. EMAIL_USER: Your email address.
-        3. EMAIL_PASS: The app-specific password you need to set up in your email account to send emails from the server.
-        4. HOST: The server address (e.g., localhost for local development).
-        5. USER: The database username.
-        6. DATABASE: The name of the database.
-        7. PORT: The port on which the database is running.
-        8. PASSWORD: The password for the database user.
-        
-        Please ensure that all these details are set in your .env file before attempting to run the website.`);
-      
-      // Set the flag to true after showing the alert
-      alertShown = true;
-    }
-  }, []);
 
   return (
     <div>
